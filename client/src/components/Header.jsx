364057711e62cfeb2d03a386c5bb7a348521b3f6
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-01-23 22:38:01 +0530

    create: navbar functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,55 @@
-import { Navbar } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 export const Header = () => {
   return (
-    
-  )
-}
+    <Navbar className="border-b-2">
+      {/* Logo of the webapp */}
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        Trial
+      </Link>
+      <form>
+        {/* for search tab */}
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+
+      {/* for small screen search button */}
+      <Button className="w-12 h-10 lg:hidden" color="grey" pill>
+        <AiOutlineSearch />
+      </Button>
+      {/* Dark/Light Mode and Sign in button */}
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="sign-in">
+          <Button color="purple">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+
+      {/* Collapse Nav bar with links */}
+      <Navbar.Collapse>
+        <Navbar.Link>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link>
+          <Link to="/message">Message</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
