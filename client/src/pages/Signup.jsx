d6d19cf6547303024a93214bba7e1fa41a04ca31
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-01-27 20:12:49 +0530

    create: sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md: items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            Trial
+          </Link>
+          <p className="text-sm mt-5">
+            I am making signup page where content is yet to be updated.
+          </p>
+        </div>
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div className="">
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div className="">
+              <Label value="Your email" />
+              <TextInput
+                type="text"
+                placeholder="name@company.com"
+                id="email"
+              />
+            </div>
+            <div className="">
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
