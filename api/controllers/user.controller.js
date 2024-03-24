b30ddf293c46fafd955ad42f1bdd0be8e0866c41
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-25 00:05:12 +0530

    add: comment show functionlity on posts

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = (req, res) => {
-  res.json({message: 'API is working'});
+  res.json({ message: 'API is working!' });
 };
 
 export const updateUser = async (req, res, next) => {
@@ -67,15 +67,18 @@ export const deleteUser = async (req, res, next) => {
 };
 
 export const signout = (req, res, next) => {
-  try{
-  res.clearCookie('access_token').status(200).json('User has been signed out');
-}
-  catch(error){
-  next(error);
-}};
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getUsers = async (req, res, next) =>{
-  if(!req.user.isAdmin){
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to see all users'));
   }
   try {
@@ -84,14 +87,14 @@ export const getUsers = async (req, res, next) =>{
     const sortDirection = req.query.sort === 'asc' ? 1 : -1;
 
     const users = await User.find()
-      .sort({ createdAt: sortDirection})
+      .sort({ createdAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
 
-    const usersWithoutPassword = users.map((user) =>{
-      const {password, ...rest} = user._doc;
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
       return rest;
-    })
+    });
 
     const totalUsers = await User.countDocuments();
 
@@ -99,7 +102,7 @@ export const getUsers = async (req, res, next) =>{
 
     const oneMonthAgo = new Date(
       now.getFullYear(),
-      now.getMonth()-1,
+      now.getMonth() - 1,
       now.getDate()
     );
     const lastMonthUsers = await User.countDocuments({
@@ -107,12 +110,24 @@ export const getUsers = async (req, res, next) =>{
     });
 
     res.status(200).json({
-      users:usersWithoutPassword,
+      users: usersWithoutPassword,
       totalUsers,
       lastMonthUsers,
-    })
-
+    });
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
