db6f0b91d3bd41aff778bc33f1041b56ec62ad95
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-24 21:34:32 +0530

    add: comment section and api to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './route/user.route.js';
 import authRoutes from './route/auth.route.js';
 import postRoutes from './route/post.route.js';
+import commentRoutes from './route/comment.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -31,6 +32,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
