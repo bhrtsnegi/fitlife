b8b9a146e391c9bbd8a2bdccc35d4604cfb27136
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-26 15:54:44 +0530

    add: edit functionality to comments

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -11,8 +11,6 @@ export default function CommentSection({ postId }) {
   const [comments, setComments] = useState([]);
   const navigate = useNavigate();
 
-  console.log(comments);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (comment.length > 200) {
@@ -59,11 +57,11 @@ export default function CommentSection({ postId }) {
   const handleLike = async (commentId) => {
     try {
       if (!currentUser) {
-        navigate('/sign-in');
+        navigate("/sign-in");
         return;
       }
       const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-        method: 'PUT',
+        method: "PUT",
       });
       if (res.ok) {
         const data = await res.json();
@@ -84,6 +82,13 @@ export default function CommentSection({ postId }) {
     }
   };
 
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
+    );
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -147,7 +152,12 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment
+              key={comment._id}
+              comment={comment}
+              onLike={handleLike}
+              onEdit={handleEdit}
+            />
           ))}
         </>
       )}
