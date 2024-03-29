add57d18f50c136935207389c15b657a29c161ff
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-03-29 15:10:06 +0530

    update: call to action button

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,7 +2,7 @@ import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl">
+    <div className="flex md:flex-row flex-col p-4 border border-teal-400 rounded-md text-center lg:pl-[20%]">
       <div className="flex-1 justify-center flex flex-col">
         <h2 className="text-2xl">Want to Hire me?</h2>
         <p className="text-gray-500 my-2">Send me an email here.</p>
@@ -19,7 +19,7 @@ export default function CallToAction() {
           </a>
         </Button>
       </div>
-      <div className="p-7 flex-1">
+      <div className="p-7 flex-1 flex md:justify-normal justify-center">
         <img src="https://bhrtsnegi.github.io/assets/imgs/avatar.jpg" />
       </div>
     </div>
